fa481afd43684640512f4111a36a52b92a95240a
athinellis <athinellis@gmail.com>
2024-11-05 10:15:50 +0200

    demo project

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,9 @@
+class WelcomeController < ApplicationController
+  def index
+   counter = Counter.find_or_create_by(name: "default")
+    counter.value = (counter.value || 0) + 1
+    counter.save!
+    render plain: "Counter #{counter.value}; #{Rails.version}/#{RUBY_VERSION}"
+  
+  end
+end
